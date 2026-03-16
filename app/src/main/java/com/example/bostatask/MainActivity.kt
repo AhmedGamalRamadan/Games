456af8443a323369d67c8af2c8d54d6b5d31ea5e
AhmedGamalRamadan <AHMED00AHMED00GAMAL@gmail.com>
2026-03-16 02:46:50 +0200

    change status bar color

--- app/src/main/java/com/example/bostatask/MainActivity.kt
+++ app/src/main/java/com/example/bostatask/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.bostatask.ui.navigation.AppNavigation
 import com.example.bostatask.ui.theme.BostaTaskTheme
@@ -12,6 +13,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.getInsetsController(window, window.decorView)
+            .isAppearanceLightStatusBars = true
         setContent {
             BostaTaskTheme {
                 val navController = rememberNavController()
